295e0ce4630f6a77b34c0966010c911464ae184f
Nimah <busayonimota1603@gmail.com>
2022-08-25 11:48:04 +0100

    [Update]: Add the prototype of 7-binary_tree_inorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -33,6 +33,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value); /* In
 void binary_tree_delete(binary_tree_t *tree); /* Deletes an entire binary tree */
 int binary_tree_is_leaf(const binary_tree_t *node); /* Checks if a node is a leaf */
 int binary_tree_is_root(const binary_tree_t *node); /*Checks if a node is root */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)); /* Traverses through a binary tree using pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)); /* Traverses through a binary tree in pre-order traversal */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)); /* Traverses through a binary tree in in-order traversal */
 
 #endif /* _BINARY_TREES_H_ */
