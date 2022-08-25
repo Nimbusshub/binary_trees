5733a66905849a0677777c6c2f927eec2082aba5
Nimah <busayonimota1603@gmail.com>
2022-08-25 23:26:41 +0100

    [Update]: Add prototype for 15-binary_tree_is_full.c

--- binary_trees.h
+++ binary_trees.h
@@ -41,6 +41,7 @@ size_t binary_tree_depth(const binary_tree_t *tree); /* Measures the depth of a 
 size_t binary_tree_size(const binary_tree_t *tree); /* Measures the size of a binary tree */
 size_t binary_tree_leaves(const binary_tree_t *tree); /* Counts the leaves in a binary tree */
 size_t binary_tree_nodes(const binary_tree_t *tree); /* Counts the node with at least 1 child in a binary tree */
+int binary_tree_is_full(const binary_tree_t *tree); /* Checks if a binary tree is full */
 
 
 #endif /* _BINARY_TREES_H_ */
